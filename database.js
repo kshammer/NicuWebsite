d319cb5dad1a7914162f338fe66bb2c4f9c28282
watsonns <watsonns@vcu.edu>
2018-02-08 19:07:06 -0500

    budget search

--- database.js
+++ database.js
@@ -2,6 +2,8 @@ $(document).ready(function () {
     var rootRef = firebase.database().ref().child("patients");
     rootRef.on("child_added", snap => {
 
+	
+	
         var name = snap.child("HeartRate").val();
 
         $("#fireP").append("HeartRate " + name + "<br>");
