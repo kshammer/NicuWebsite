7d13d90fe9a2fa474d3e4aef88c1158c4027bcc9
kshammer <hammerschmiks@vcu.edu>
2018-03-28 15:38:14 -0400

    updated historical php:

--- historical.php
+++ historical.php
@@ -30,16 +30,35 @@ if(! $retval ) {
 }
 if ($retval->num_rows > 0) {
     // output data of each row
+    echo "<center>";
+    echo "<style>".
+    "table, th, td {".
+        "border: 1px solid black;".
+        "background-color: white;".
+    "}".
+    "</style>";
+    echo "<table>";
+    echo " <tr>".
+    "<th>behavior</th>".
+    "<th>cardiovascular</th>". 
+    "<th>respiratory</th>".
+    "<th>abdominal</th>".
+    "<th> feeds</th>".
+    "<th> formula</th>".
+  "</tr>";
     while($row = $retval->fetch_assoc()) {
-        echo "
-        <br>behavior :{$row['behavior']}  <br> ".
-        "cardiovascular : {$row['cardiovascular']} <br> ".
-        "respiratory : {$row['respiratory']} <br> ".
-        "abdominal : {$row['abdominal']}<br> ".
-        "feeds : {$row['feeds']}<br> ".
-        "forumla : {$row['formula']}<br> ".
-        "--------------------------------<br>";
+        echo "<tr>
+        <td>{$row['behavior']}  </td> ".
+        "<td>{$row['cardiovascular']} </td> ".
+        "<td> {$row['respiratory']} </td> ".
+        "<td>{$row['abdominal']}</td> ".
+        "<td> {$row['feeds']}</td> ".
+        "<td> {$row['formula']}</td> ".
+        "
+        </tr>";
     }
+    echo "</table>";
+    echo "</center>";
 } else {
     echo "0 results";
 }
